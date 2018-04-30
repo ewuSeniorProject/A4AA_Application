b89f03e2c0955c8bae89ba26367505f0245f128a
BlakeNoel <blakeenoel@gmail.com>
2018-04-30 11:09:27 -0700

    Revert "Revert "refactoring + some section B stuff""
    This reverts commit bb3fd19bd14c500027e88b0e89fd4d025927c80b.

---
+++ A4AA_Application/SurveyClasses/SurveyQuestions/misc/Use_Use_Rol.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using A4AA_Application.SurveyClasses.SurveyAnswers;
+
+namespace A4AA_Application.SurveyClasses.SurveyQuestions
+{
+    public class Use_Use_Rol : Question
+    {
+        public Use_Use_Rol()
+        {
+            TheAnswer = new A_Text(128);
+            QuestionText = "User Role:";
+        }
+        //NEED SOME OBJECT OR LOGIC HERE WHICH WILL PROVIDE USER WITH DROP DOWN OF CORRECT POTENTIAL OPTIONS
+    }
+}
