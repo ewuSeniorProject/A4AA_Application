3a2477f2625ebd739e6b2ebf87eab6987212486f
jennasaj <jenna.jasa@gmail.com>
2018-04-30 10:46:34 -0700

    added section D-F pages

---
+++ A4AA_Application/SurveyClasses/SurveyQuestions/SectionD/Int_doo_ope_cle.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace A4AA_Application.SurveyClasses.SurveyQuestions.SectionD
+{
+    class Int_doo_ope_cle : Question
+    {
+    }
+}
---
+++ A4AA_Application/SurveyPages/SurveySectionD.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using A4AA_Application.SurveyClasses;
+using A4AA_Application.SurveyClasses.SurveyQuestions;
+using A4AA_Application.SurveyClasses.SurveySections;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace A4AA_Application.SurveyPages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SurveySectionD : ContentPage
+	{
+		private Survey theSurvey;
+		private SectionD SecD;
+
+		public SurveySectionD(Survey theSurvey)
+		{
+
+			this.theSurvey = theSurvey;
+			SectionD = theSurvey.SectionD;
+			InitializeComponent();
+			Title = "Section D";
+
+			var layout = this.FindByName<StackLayout>("theStackLayout");
+		}
+	}
+}
