0b832bdacfc706df0b3f8c225dcb05548c7322bb
BlakeNoel <blakeenoel@gmail.com>
2018-05-21 08:57:24 -0700

    stuff

--- A4AA_Application/SurveyClasses/SurveyQuestions/SectionI/Restroom_InfoQ/Res_Inf_Soa_Dis.cs
+++ A4AA_Application/SurveyClasses/SurveyQuestions/SectionI/Restroom_InfoQ/Res_Inf_Soa_Dis.cs
@@ -5,8 +5,8 @@ using A4AA_Application.SurveyClasses.SurveyAnswers;
 
 namespace A4AA_Application.SurveyClasses.SurveyQuestions
 {
-    public class Res_Inf_Soa : YesNoQuestion
+    public class Res_Inf_Soa_Dis : YesNoQuestion
     {
-        public Res_Inf_Soa() : base("The height of the soap dispenser control is 48\" or less from the floor:") { }
+        public Res_Inf_Soa_Dis() : base("The height of the soap dispenser control is 48\" or less from the floor:") { }
     }
 }
