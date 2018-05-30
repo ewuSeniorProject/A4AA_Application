8783cf57903e97ca5d0e5d8035a4c02378febdfe
jennasaj <jenna.jasa@gmail.com>
2018-05-30 02:17:24 -0700

    fixed Page B

--- A4AA_Application/SurveyPages/SurveySectionB.xaml.cs
+++ A4AA_Application/SurveyPages/SurveySectionB.xaml.cs
@@ -12,55 +12,50 @@ namespace A4AA_Application.SurveyPages
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class SurveySectionB : ContentPage
 	{
-        private Survey theSurvey;
-        private ArrayList QuestionLabels;
-        private ArrayList QuestionAnswerSpaces;
-        private ParkingT table1;
-        private Route_From_ParkingT table2;
-        private Passenger_LoadingT table3;
-        private STA_BusT table4;
-       
+		private Survey theSurvey;
+		private ArrayList QuestionLabels;
+		private ArrayList QuestionAnswerSpaces;
+		private ParkingT table1;
+		private Route_From_ParkingT table2;
+		private Passenger_LoadingT table3;
+		private STA_BusT table4;
 
 
-        public SurveySectionB (Survey theSurvey)
+
+		public SurveySectionB(Survey theSurvey)
 		{
-            this.theSurvey = theSurvey;
-			InitializeComponent ();
-            this.Title = "Parking, Loading Zones, Bus Stops";
-            var layout = this.FindByName<StackLayout>("theStackLayoutB");
+			this.theSurvey = theSurvey;
+			InitializeComponent();
+			this.Title = "Parking, Loading Zones, Bus Stops";
+			var layout = this.FindByName<StackLayout>("theStackLayoutB");
 
-            QuestionLabels = new ArrayList();
-            QuestionAnswerSpaces = new ArrayList();
+			QuestionLabels = new ArrayList();
+			QuestionAnswerSpaces = new ArrayList();
 
-            table1 = theSurvey.sectionB.ParkingT;
-            table2 = theSurvey.sectionB.Route_From_ParkingT;
-            table3 = theSurvey.sectionB.Passenger_LoadingT;
-            table4 = theSurvey.sectionB.STA_BusT;
-            //table5 = theSurvey.SectionB.STA_RouteT;
+			table1 = theSurvey.sectionB.ParkingT;
+			table2 = theSurvey.sectionB.Route_From_ParkingT;
+			table3 = theSurvey.sectionB.Passenger_LoadingT;
+			table4 = theSurvey.sectionB.STA_BusT;
+			//table5 = theSurvey.SectionB.STA_RouteT;
 
-            PropertyInfo[] properties = typeof(ParkingT).GetProperties();
+			PropertyInfo[] properties = typeof(ParkingT).GetProperties();
 
 			generateFields(properties, table1);
 
-            properties = typeof(Route_From_ParkingT).GetProperties();
+			properties = typeof(Route_From_ParkingT).GetProperties();
 			generateFields(properties, table2);
 
-            properties = typeof(Passenger_LoadingT).GetProperties();
+			properties = typeof(Passenger_LoadingT).GetProperties();
 			generateFields(properties, table3);
 
-            properties = typeof(STA_BusT).GetProperties();
+			properties = typeof(STA_BusT).GetProperties();
 			generateFields(properties, table4);
 
 			for (int i = 0; i < QuestionLabels.Count; i++)
-            {
-                layout.Children.Add((Label)QuestionLabels[i]);
-                layout.Children.Add((View)QuestionAnswerSpaces[i]);
-            }
-
-            
-        }
-
-
+			{
+				layout.Children.Add((Label)QuestionLabels[i]);
+				layout.Children.Add((View)QuestionAnswerSpaces[i]);
+			}
 
 			//Adding submit button
 			Button Sub_Button = new Button { Text = "Submit" };
@@ -100,83 +95,83 @@ namespace A4AA_Application.SurveyPages
 					EntryType(e, q);
 				}
 			}
-    }
+		}
 
-        private void EntryType(Entry ent, Question q)
-        {
-            QuestionAnswerSpaces.Add(ent);
-            ent.Unfocused += (sender, e) => Ans_Completed(sender, e, q); ;
-        }
+		private void EntryType(Entry ent, Question q)
+		{
+			QuestionAnswerSpaces.Add(ent);
+			ent.Unfocused += (sender, e) => Ans_Completed(sender, e, q); ;
+		}
 
-        private void Ans_Completed(object sender, EventArgs e, Question q)
-        {
-            try
-            {
-                q.TheAnswer.setAnswer(((Entry)sender).Text);
-                q.HasBeenAnswered = true;
-            }
-            catch (Exception)
-            {
-                DisplayAlert("Error", q.TheAnswer.getErrorMessage(), "OK");
-            }
-        }
+		private void Ans_Completed(object sender, EventArgs e, Question q)
+		{
+			try
+			{
+				q.TheAnswer.setAnswer(((Entry)sender).Text);
+				q.HasBeenAnswered = true;
+			}
+			catch (Exception)
+			{
+				DisplayAlert("Error", q.TheAnswer.getErrorMessage(), "OK");
+			}
+		}
 
-        private void SelectedDate(object sender, DateChangedEventArgs e, Question q)
-        {
-            try
-            {
-                q.TheAnswer.setAnswer(((DatePicker)sender).Date.ToShortDateString());
-                q.HasBeenAnswered = true;
-            }
-            catch (Exception)
-            {
-                DisplayAlert("Error", "Unforseen error.", "OK");
-            }
-        }
+		private void SelectedDate(object sender, DateChangedEventArgs e, Question q)
+		{
+			try
+			{
+				q.TheAnswer.setAnswer(((DatePicker)sender).Date.ToShortDateString());
+				q.HasBeenAnswered = true;
+			}
+			catch (Exception)
+			{
+				DisplayAlert("Error", "Unforseen error.", "OK");
+			}
+		}
 
-        private void AddToPicker(Question q, Picker p)
-        {
-            QuestionAnswerSpaces.Add(p);
-            foreach (string s in q.Options)
-            {
-                p.Items.Add(s);
-            }
+		private void AddToPicker(Question q, Picker p)
+		{
+			QuestionAnswerSpaces.Add(p);
+			foreach (string s in q.Options)
+			{
+				p.Items.Add(s);
+			}
 
-        }
+		}
 
-        private Label CreateLabel(Question q)
-        {
-            Label l = new Label { Text = q.QuestionText, HorizontalTextAlignment = TextAlignment.Center, FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)) };
+		private Label CreateLabel(Question q)
+		{
+			Label l = new Label { Text = q.QuestionText, HorizontalTextAlignment = TextAlignment.Center, FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)) };
 
-            return l;
+			return l;
 
-        }
+		}
 
-        private Picker genPicker()
-        {
-            return new Picker { Title = "Select one" };
-        }
+		private Picker genPicker()
+		{
+			return new Picker { Title = "Select one" };
+		}
 
-        private Picker genCustomPicker(Question q)
-        {
-            return new Picker { Title = q.TheAnswer.getAnswer() };
-        }
+		private Picker genCustomPicker(Question q)
+		{
+			return new Picker { Title = q.TheAnswer.getAnswer() };
+		}
 
-        //Events
-        private void SelectedIndexChanged(object sender, EventArgs e, Question q)
-        {
-            try
-            {
-                q.TheAnswer.setAnswer(((Picker)sender).SelectedItem.ToString());
-                q.HasBeenAnswered = true;
-            }
-            catch (Exception)
-            {
-                DisplayAlert("Error", "Unforseen error.", "OK");
-            }
-        }
+		//Events
+		private void SelectedIndexChanged(object sender, EventArgs e, Question q)
+		{
+			try
+			{
+				q.TheAnswer.setAnswer(((Picker)sender).SelectedItem.ToString());
+				q.HasBeenAnswered = true;
+			}
+			catch (Exception)
+			{
+				DisplayAlert("Error", "Unforseen error.", "OK");
+			}
+		}
 
-    }
+
 
 
 		public async void Sub_but_clicked(object sender, EventArgs args)
@@ -191,5 +186,6 @@ namespace A4AA_Application.SurveyPages
 				await Navigation.PushAsync(new MainSurveyPage(theSurvey));
 
 		}
+
 	}
 }
